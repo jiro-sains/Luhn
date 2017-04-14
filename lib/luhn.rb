a96a1a305fcece1658da4ad326c7d0319ce3ba06
jiro-sains <jrosains@gmail.com>
2017-04-14 13:03:46 +0000

    after mentor session

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,15 +1,19 @@
 module Luhn
-	def self.is_valid?(number)
-		digits = []
-		operated_digits = []
-		number.to_s.split('').each {|i| digits << i.to_i}
-		reversed_digits = digits.reverse
-		reversed_digits.each_with_index do |val, i|
-			val = val*2 if i%2 == 1
+
+	def self.calculations(digits, operated_digits = [])
+		digits.reverse.each_with_index do |val, i|
+			val = val * 2 if i % 2 == 1
 			val = val - 9 if val >= 10
 			operated_digits << val
 		end
-		return operated_digits.inject(:+) % 10 == 0 
+		operated_digits
+	end
+
+	def self.is_valid?(number) 
+    #check_sum(operation(reverse(split_number(number)))
+		#number.to_s.split('').each {|i| digits << i.to_i}
+		digits = number.to_s.split('').map(&:to_i)
+		calculations(digits).inject(:+) % 10 == 0 
 	end
 end
 
